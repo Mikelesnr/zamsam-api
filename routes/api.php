11bff3066f07ed840e8fc1013b3ad02689c78fd1
Michael Mwanza <mwanza.n.m@gmail.com>
2025-11-07 15:35:52 +0200

    mail functionality added

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\MailController;
+
+Route::post('/contact', [MailController::class, 'send']);
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
